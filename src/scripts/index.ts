66f992782d022c8145383ef0db1c5485ee9f672b
Greyson Elkins <greysonelkins@gmail.com>
2021-07-04 13:03:22 -0600

    Add script for determining action for a given day

---
+++ src/scripts/index.ts
@@ -0,0 +1,14 @@
+import { Day } from 'types'
+
+type Action = { start: Day | number, action: 'vote' | 'submit' }
+export const voting:Action = { start: Day.MON, action: 'vote' }
+export const submission:Action = { start: Day.THU, action: 'submit' }
+
+export const ruleOfTheDay = (day: Day | number): 'vote' | 'submit' => {
+  const events = [voting, submission].sort((a, b) => a.start - b.start)
+  if (day < events[0].start) return events[1].action
+  if (events[0].start <= day && day < events[1].start) return events[0].action
+  if (day >= events[1].start) return events[1].action
+  console.error('ruleOfTheDay encountered an unexpected value')
+  return 'vote'
+}
