9b1b5a1b8ea1a5c25d5f90e2ffcbb8c72f5b15bf
Greyson Elkins <greysonelkins@gmail.com>
2021-07-05 16:45:56 -0600

    Move Toast up

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import { UserProvider } from 'hooks'
 import { App } from 'components';
 import './index.scss';
+import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 ReactDOM.render(
   <BrowserRouter>
     <UserProvider>
       <App />
     </UserProvider>
+    <ToastContainer position="top-right" />
   </BrowserRouter>,
   document.getElementById('root')
-);
+)
 
 serviceWorkerRegistration.register();
 
