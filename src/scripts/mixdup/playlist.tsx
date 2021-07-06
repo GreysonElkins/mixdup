b3b710de230cc329f5d82ea3ac4b83acf483f960
Greyson Elkins <greysonelkins@gmail.com>
2021-07-05 19:53:00 -0600

    Parse JSON playlist data

--- src/scripts/mixdup/playlist.tsx
+++ src/scripts/mixdup/playlist.tsx
@@ -5,8 +5,9 @@ export const getAllPlaylists = async () => {
   try {
     const token = await firebase.auth().currentUser?.getIdToken()
     if (!token) return console.error('You must log in to get all playlists')
-    const playlistData = await fetch(`${baseUrl}/all-playlists`)
-    playlistData.text().then(text => console.log(text))
+    const playlistData = await fetch(`${baseUrl}/all-playlists`, { headers: {Authorization: `Bearer ${token}`}})
+    const data = await playlistData.text()
+    return JSON.parse(data)
   } catch (error) {
     console.error(error)
   }
