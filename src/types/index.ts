31482b657dabcd6a180c6d8d4546f0e2f21379e8
Greyson Elkins <greysonelkins@gmail.com>
2021-07-05 19:53:25 -0600

    Add FixedLengthArray type

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,5 +11,19 @@ export type RouteType = {
   }
 }
 
-export type { SpotifyTokens, UserTokenUpdate, UserContextType } from './contexts.d'
+type ArrayLengthMutationKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift' | number
+type ArrayItems<T extends Array<any>> = T extends Array<infer TItems> ? TItems : never
+export type FixedLengthArray<T extends any[]> = Pick<T, Exclude<keyof T, ArrayLengthMutationKeys>> & {
+  [Symbol.iterator]: () => IterableIterator<ArrayItems<T>>
+}
+
+export type { SpotifyPlaylist, TrackItem } from './spotifyData.d'
 export { Day } from './contexts.d'
+export type {
+  SpotifyTokens,
+  UserTokenUpdate,
+  UserContextType,
+  LeagueContextType,
+  CalendarContextType,
+  DayAction,
+} from './contexts.d'
