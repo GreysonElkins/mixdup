27c98d81048e5ff2a59762e13dc923cde57b0dc0
Greyson Elkins <greysonelkins@gmail.com>
2021-07-05 19:52:39 -0600

    Add text / gallery to landing page

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import { RouteType } from 'types'
+import React, { useState, useEffect } from 'react'
+import { toast } from 'react-toastify' 
 import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+import { getAllPlaylists } from 'scripts'
+import { RouteType, SpotifyPlaylist } from 'types'
+import './LandingPage.scss'
 
 import { CalendarGraphic, PlaylistGallery } from 'components'
 const LandingPage: React.FC = () => {
+  const [playlists, setPlaylists] = useState<SpotifyPlaylist[]>([])
+
+  useEffect(() => {
+    getAllPlaylists().then((playlistData) => {
+      const { items: playlists } = playlistData || {}
+      playlists
+        ? setPlaylists(playlists)
+        : toast.dark('Something went wrong getting playlists, please try again')
+    })
+  }, [])
+
   return (
-    <>
+    <div className="LandingPage">
+      <h2>How it Works</h2>
       <CalendarGraphic />
-      {/* <PlaylistGallery /> */}
-    </>
+      <div>
+        Every week the mix-league makes a new playlist together and votes on their favorites
+        together. Each contributor gets to rank their top three songs, but you can't pick songs
+        you've heard before.
+      </div>
+      {playlists && (
+        <>
+          <h2 className="galleryTitle">All Mixdup Playlists</h2>
+          <PlaylistGallery playlists={playlists} />
+        </>
+      )}
+    </div>
   )
 } 
 
