968d5bd9b53f31a0a1ea1c5f5af317acf3e17a09
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 16:32:34 -0600

    Add submit song script

--- src/scripts/mixdup/playlist.tsx
+++ src/scripts/mixdup/playlist.tsx
@@ -1,5 +1,6 @@
 import { firebase } from 'scripts'
 import { baseUrl } from '.'
+import { toast } from 'react-toastify'
 
 export const getAllPlaylists = async () => {
   try {
@@ -10,5 +11,37 @@ export const getAllPlaylists = async () => {
     return JSON.parse(data)
   } catch (error) {
     console.error(error)
+  }
+}
+
+export const submitSong = async (user_id: string, submission_uri: string, trackName: string, date: Date) => {
+  try {
+    debugger
+    const token = await firebase.auth().currentUser?.getIdToken()
+    await fetch(`${baseUrl}/submit-song`, {
+      method: 'POST',
+      headers: {
+        'Access-Control-Request-Headers': 'Content-Type, authorization',
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ user_id, submission_uri, trackName, date }),
+    }).then((response) => {
+      if (response.ok) {
+        toast.success('Your submission for the next playlist has been received!', {
+          toastId: 'playlist submission',
+        })
+      } else {
+        console.error(response)
+        if (response.status === 429)
+          return toast.dark('You already submitted a song for this weeks playlist!')
+        toast.dark('Something went wrong, try that again.', {
+          toastId: 'playlist submission error',
+        })
+      }
+    })
+  } catch (error) {
+    console.error(error)
+    toast.dark('Something went wrong, try that again.', { toastId: 'submission api error' })
   }
 }
