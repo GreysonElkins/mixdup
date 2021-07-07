f2690bcf4cddf9f201202da0bdcde18bca82cd47
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 17:18:33 -0600

    Add controls for calendar context

--- src/hooks/context/Calendar.tsx
+++ src/hooks/context/Calendar.tsx
@@ -1,12 +1,24 @@
-import { createContext, useContext } from 'react'
+import { createContext, useState, useContext } from 'react'
 import { CalendarContextType } from 'types'
+
+import { AdminCalendarControl } from 'components'
 
 const CalendarContext = createContext({} as CalendarContextType)
 
 export const CalendarProvider: React.FC = ({ children }) => {
+  const [dateOverride, setDateOverride] = useState<Date | null>(null)
+  const [isAdminMode, setIsAdminMode] = useState<boolean>(true)
   const today = new Date()
 
-  return <CalendarContext.Provider value={{ today }}>{children}</CalendarContext.Provider>
+  return (
+    <CalendarContext.Provider value={{ today: dateOverride || today, isAdminMode }}>
+      {isAdminMode && <AdminCalendarControl
+        isDateOverride={dateOverride !== null}
+        setDateOverride={setDateOverride}
+      />}
+      {children}
+    </CalendarContext.Provider>
+  )
 }
 
 export const useCalendar = () => useContext(CalendarContext)
