df13d3189abdd598f948d87fe34473af8a7851e3
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 21:53:10 -0600

    Fix dates, get playlist in League context

--- src/hooks/context/League.tsx
+++ src/hooks/context/League.tsx
@@ -1,20 +1,31 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import { getDate, getDay } from 'date-fns'
+import { getDay } from 'date-fns'
 import { ruleOfTheDay } from 'scripts'
-import { DayAction, Day, LeagueContextType } from 'types'
+import { DayAction, Day, LeagueContextType, SpotifyPlaylist } from 'types'
+import { getRelevantPlaylist } from 'scripts'
 import { useCalendar } from 'hooks'
 
 const LeagueContext = createContext({} as LeagueContextType)
 
 export const LeagueProvider: React.FC = ({ children }) => {
-  const [mode, setMode] = useState<'vote' | 'submit' | 'loading'>('loading')
+  const [mode, setMode] = useState<'vote' | 'submit' | 'loading'>('loading') // might not need loading
+  const [thisWeeksPlaylist, setThisWeeksPlaylist] = useState<SpotifyPlaylist | undefined>()
   const [votingStarts] = useState<DayAction>({ start: Day.WED, action: 'vote' })
   const [submissionsStart] = useState<DayAction>({ start: Day.SUN, action: 'submit' })
   const { today } = useCalendar()
 
   useEffect(() => {
+    getRelevantPlaylist(today)
+      .then(playlist => {
+        playlist && setThisWeeksPlaylist(playlist)
+        console.log(playlist)
+      })
+      .catch(error => console.error(error))
+  }, [today])
+
+  useEffect(() => {
     const rule = ruleOfTheDay({
-      day: getDate(today),
+      day: getDay(today),
       actions: [votingStarts, submissionsStart],
     })
     setMode(rule)
@@ -22,7 +33,7 @@ export const LeagueProvider: React.FC = ({ children }) => {
 
   return (
     <LeagueContext.Provider
-      value={{ mode, loading: mode === 'loading', rules: [votingStarts, submissionsStart] }}
+      value={{ mode, loading: mode === 'loading', rules: [votingStarts, submissionsStart], thisWeeksPlaylist }}
     >
       {children}
     </LeagueContext.Provider>
