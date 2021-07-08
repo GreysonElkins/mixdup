c9c95d1efaf7de9ffd5101316bc2a7f46a98214e
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 18:13:55 -0600

    Add Day enum translator

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -17,6 +17,25 @@ export const ruleOfTheDay = ({
   return 'vote'
 }
 
+export const dayFromDayNum = (num: Day) => {
+  switch (num) {
+    case 0:
+      return 'Sunday'
+    case 1:
+      return 'Monday'
+    case 2: 
+      return 'Tuesday'
+    case 3: 
+      return 'Wednesday'
+    case 4:
+      return 'Thursday'
+    case 5:
+      return 'Friday'
+    case 6:
+      return 'Saturday' 
+  }
+}
+
 export { default as firebase, auth } from './firebase/firebaseConfig'
 export { signIn, disconnectUser } from './firebase/user'
 export { getAllPlaylists, submitSong } from './mixdup/playlist'
