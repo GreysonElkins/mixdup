e8cd4082c5d3f0e845f2c61a5bfe54d8fd3d26fd
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 21:54:31 -0600

    Render playlist on league page

--- src/views/League/index.tsx
+++ src/views/League/index.tsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect, ReactElement } from 'react'
-import { RouteType } from 'types'
+import { RouteType, SpotifyPlaylist } from 'types'
 import { faAward } from '@fortawesome/free-solid-svg-icons'
 
 import { useLeague } from 'hooks'
-import { HowItWorks } from 'components'
+import { Songlist, HowItWorks } from 'components'
 import Submissions from './Submissions'
 import { HelpIcon } from 'style'
 
 const LeaguePage: React.FC = () => {
-  const [View, setView] = useState<ReactElement | undefined>(undefined)
-  const { mode } = useLeague()
+  const [View, setView] = useState<ReactElement<{playlist?: SpotifyPlaylist}> | undefined>(undefined)
+  const { mode, thisWeeksPlaylist } = useLeague()
   
   useEffect(() => {
-    mode === 'vote' ? setView(undefined) : setView(<Submissions />) 
+    mode === 'submit' && setView(<Submissions />) 
+    thisWeeksPlaylist && mode === 'vote' && setView(<Songlist playlist={thisWeeksPlaylist}/>) // this should be broken out into another view with player and votes
   }, [mode])
 
   return (
     <>
       {View && View}
       <HelpIcon modal={HowItWorks} />
+      {/* HelpIcon is throwing an error */}
     </>
   )
 }
