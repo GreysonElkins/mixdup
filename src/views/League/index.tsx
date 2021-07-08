fac4db41143bdb789329b0e221dd18e092dc7b5e
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 20:36:57 -0600

    Start rendering league differently per day

--- src/views/League/index.tsx
+++ src/views/League/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect, ReactElement } from 'react'
 import { RouteType } from 'types'
 import { faAward } from '@fortawesome/free-solid-svg-icons'
 
+import { useLeague } from 'hooks'
 import { HowItWorks } from 'components'
 import Submissions from './Submissions'
 import { HelpIcon } from 'style'
 
-const LeaguePage: React.FC = () => (
-  <>
-    <Submissions />
-    <HelpIcon modal={HowItWorks} />
-  </>
-)
+const LeaguePage: React.FC = () => {
+  const [View, setView] = useState<ReactElement | undefined>(undefined)
+  const { mode } = useLeague()
+  
+  useEffect(() => {
+    mode === 'vote' ? setView(undefined) : setView(<Submissions />) 
+  }, [mode])
+
+  return (
+    <>
+      {View && View}
+      <HelpIcon modal={HowItWorks} />
+    </>
+  )
+}
 
 const league: RouteType = {
   path: '/mix-league',
