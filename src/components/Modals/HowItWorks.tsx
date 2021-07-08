aaaa565f4733a102faa91689acfe5e00fa93e8bd
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 21:51:50 -0600

    Update copy

--- src/components/Modals/HowItWorks.tsx
+++ src/components/Modals/HowItWorks.tsx
@@ -11,7 +11,7 @@ const HowItWorks = ()  => {
       <h2>How it Works</h2>
       <CalendarGraphic />
       <div>
-        Every week the mix-league makes a new playlist together and votes on their favorites
+        Every week the mix-league makes a new collaborative playlist and votes on their favorites
         together. Each contributor gets to rank their top three songs, but you can't pick songs
         you've heard before.
       </div>
