3cb5bf4cf20353e086430f3a15437f52015397dc
Greyson Elkins <greysonelkins@gmail.com>
2021-07-07 19:00:44 -0600

    Undo Modal tests

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Switch } from 'react-router-dom'
 
 import RouteMap from 'views'
-import { useUser, useCalendar, useModal } from 'hooks'
+import { useUser, useCalendar } from 'hooks'
 import { LoginPage } from 'views/Login'
-import { Navigation, HowItWorks } from 'components'
+import { Navigation } from 'components'
 import { SpinningLoader } from 'style'
 
 import './App.scss'
@@ -12,9 +12,6 @@ import './App.scss'
 const App: React.FC = () => {
   const { isLoggedIn, loading } = useUser()
   const { isAdminMode } = useCalendar()
-  const { setModal } = useModal()
-
-  useEffect(() => setModal(HowItWorks), [])
   
   if (loading) return <SpinningLoader solo />
   if (!isLoggedIn) return <LoginPage />
