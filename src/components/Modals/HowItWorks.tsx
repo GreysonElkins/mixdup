2ac0555912708b2302ff164685f3a8837c6b65b4
Greyson Elkins <greysonelkins@gmail.com>
2021-07-09 11:57:50 -0600

    Remove unused hook

--- src/components/Modals/HowItWorks.tsx
+++ src/components/Modals/HowItWorks.tsx
@@ -1,22 +1,17 @@
-import React from "react"
-import { useModal } from "hooks"
 import { CalendarGraphic } from "components"
 
 import './HowItWorks.scss'
 
-const HowItWorks = ()  => {
-  const { closeModal } = useModal()
-  return (
-    <div className="HowItWorks" onClick={closeModal}>
-      <h2>How it Works</h2>
-      <CalendarGraphic />
-      <div>
-        Every week the mix-league makes a new collaborative playlist and votes on their favorites
-        together. Each contributor gets to rank their top three songs, but you can't pick songs
-        you've heard before.
-      </div>
+const HowItWorks = ()  => (
+  <div className="HowItWorks">
+    <h2>How it Works</h2>
+    <CalendarGraphic />
+    <div>
+      Every week the mix-league makes a new collaborative playlist and votes on their favorites
+      together. Each contributor gets to rank their top three songs, but you can't pick songs
+      you've heard before.
     </div>
-  )
-}
+  </div>
+)
 
 export default HowItWorks
