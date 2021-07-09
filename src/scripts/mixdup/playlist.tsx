0872153d6b135531de28c20222baf74e9f86b16c
Greyson Elkins <greysonelkins@gmail.com>
2021-07-09 11:58:56 -0600

    Add getPlaylist

--- src/scripts/mixdup/playlist.tsx
+++ src/scripts/mixdup/playlist.tsx
@@ -6,8 +6,28 @@ import { SpotifyPlaylist } from 'types'
 export const getAllPlaylists = async () => {
   try {
     const token = await firebase.auth().currentUser?.getIdToken()
-    if (!token) return console.error('You must log in to get all playlists')
+    if (!token) return toast.dark('You must log in to get all playlists')
     const playlistData = await fetch(`${baseUrl}/all-playlists`, { headers: {Authorization: `Bearer ${token}`}})
+    const data = await playlistData.text()
+    return JSON.parse(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getPlaylist = async (playlist_id: string) => {
+  try {
+    const token = await firebase.auth().currentUser?.getIdToken()
+    if (!token) return toast.dark('You must log in to get all playlists')
+    const playlistData = await fetch(`${baseUrl}/get-playlist`, {
+      method: 'POST',
+      headers: {
+        'Access-Control-Request-Headers': 'Content-Type, authorization',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ playlist_id }),
+    })
     const data = await playlistData.text()
     return JSON.parse(data)
   } catch (error) {
