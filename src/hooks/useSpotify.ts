47209c6d6243b1384d75d608ad901b798e3643d9
Greyson Elkins <greysonelkins@gmail.com>
2021-07-16 22:47:01 -0600

    Initial commit

---
+++ src/hooks/useSpotify.ts
@@ -0,0 +1,36 @@
+import { toast } from 'react-toastify'
+import { getRefreshedAuth as refreshedAuth, followPlaylist as follow } from 'scripts'
+import { useUser } from 'hooks'
+
+export const getRefreshedAuth = refreshedAuth
+
+const useSpotify = () => {
+  const { access_token, refresh_token, updateUserTokens } = useUser()
+
+  const checkAction = async (
+    result: any,
+    action: (token: string, val?: any) => void,
+    param?: any
+  ) => {
+    if (result.status >= 200 && result.status < 300) return result
+    if (result.status === 401) {
+      const new_token = await getRefreshedAuth({ access_token, refresh_token }, updateUserTokens)
+      new_token
+        ? action(new_token, param)
+        : toast.dark('Something went wrong', { toastId: 'error after auth refresh' })
+    }
+  }
+
+  const followPlaylist = async (playlist_id: string) => {
+    try {
+      const result = await follow(access_token, playlist_id)
+      return checkAction(result, follow, playlist_id)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { followPlaylist }
+}
+
+export default useSpotify
