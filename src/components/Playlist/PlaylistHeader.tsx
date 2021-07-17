0e9374c0138c1fb4b9f31a9406384abae240cf6a
Greyson Elkins <greysonelkins@gmail.com>
2021-07-16 22:46:50 -0600

    Add follow playlist button

--- src/components/Playlist/PlaylistHeader.tsx
+++ src/components/Playlist/PlaylistHeader.tsx
@@ -1,29 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import { useScrollPosition } from 'hooks'
-import { EffectProps } from 'types'
+import { FollowPlaylistButton } from 'components'
 import './Playlist.scss'
 
 type Props = {
   primary_color: string
   name: string
+  id: string
   description: string
   images: any[]
 }
 
-const PlaylistHeader:React.FC<Props> = ({ primary_color, name, description, images }) => {
+const PlaylistHeader:React.FC<Props> = ({ id, primary_color, name, description, images }) => {
   return (
     <div className="head">
       <div>
         <h1 style={{ color: primary_color }}>{name}</h1>
         <h2>{description.replace('&#x27;', "'")}</h2>
       </div>
-      {images.length > 0 && (
-        <img
+      <div className="right-head">
+        {images.length > 0 && (
+          <img
           src={images[1]?.url || images[0].url}
           alt={`A mosaic of album artwork from the playlist ${name}`}
-        />
-      )}
+          />
+          )}
+        <FollowPlaylistButton playlist_id={id} />
+      </div>
     </div>
   )
 }
