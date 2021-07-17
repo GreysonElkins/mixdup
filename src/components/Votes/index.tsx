34bf11885fdfb91648f4bbe6be5568aa70c4e72d
Greyson Elkins <greysonelkins@gmail.com>
2021-07-17 17:04:45 -0600

    Add heard fields

--- src/components/Votes/index.tsx
+++ src/components/Votes/index.tsx
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { Formik, Form, Field, FieldArray } from 'formik'
+import React from 'react'
+import { Formik, Form, FieldArray, FieldArrayRenderProps } from 'formik'
 import * as yup from 'yup'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAward, faPlusCircle, faVoteYea } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAward,
+  faPlusCircle,
+  faVoteYea,
+  faAssistiveListeningSystems,
+  faTimesCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { useCalendar, useUser } from 'hooks'
 import { submitVotes } from 'scripts'
@@ -18,25 +24,39 @@ export type VoteValues = {
 }
   
 const Selector: React.FC<{
-  place: 'first' | 'second' | 'third'
+  place: 'first' | 'second' | 'third' | 'heard'
   songs: TrackItem[]
-  values: VoteValues
   setFieldValue: (field: string, value: string) => void
-}> = ({ place, songs, values, setFieldValue,  }) => {
-    
+  index?: number 
+}> = ({ place, songs, setFieldValue, index }) => {
+
   return (
-    <div key={`${place}=select`} className="vote-selector">
-      <FontAwesomeIcon
-        icon={faAward}
-        color={place === 'first' ? '#78a3ad' : place === 'second' ? '#bc4b51' : '#d6ba73'}
-        className="ribbon-icon"
-      />
+    <div key={`${place}-select-${index}`} className="vote-selector">
+      {(place !== 'heard' || index === 0) && (
+        <FontAwesomeIcon
+          icon={place === 'heard' ? faAssistiveListeningSystems : faAward}
+          color={
+            place === 'first'
+              ? '#78a3ad'
+              : place === 'second'
+              ? '#bc4b51'
+              : place === 'third'
+              ? '#d6ba73'
+              : '#aaa'
+          }
+          className="ribbon-icon"
+        />
+      )}
       <select
-        placeholder={`${place} place`}
         name={place}
-        onChange={(event) => setFieldValue(place, event.target.value)}
+        onChange={(event) => {
+          setFieldValue(place !== 'heard' ? place : `heard[${index}]`, event.target.value)
+        }}
       >
-        <option value="" label={`${place} place`} />
+        <option
+          value=""
+          label={place === 'heard' ? `songs you've heard before` : `${place} place`}
+        />
         {songs.map(({ name }, i) => (
           <option value={name} label={name} key={`${place}-${name}-${i}`} />
         ))}
@@ -48,28 +68,81 @@ const Selector: React.FC<{
 const Votes: React.FC<{songs: TrackItem[]}> = ({ songs }) => {
   const { id } = useUser()
   const { today } = useCalendar()
-  const [closed, setClosed] = useState<boolean>(false)
-  // const printHeardFields = (value, ) => {
-      // }
-  return (
-    <Formik
-      initialValues={{ first: '', second: '', third: '', heard: [] }}
-      onSubmit={(values) => submitVotes(id, values, today)}
-    >
-      {({ values, setFieldValue }) => (
-        <Form className='Votes'>
-          <Selector place={'first'} songs={songs} values={values} setFieldValue={setFieldValue} />
-          <Selector place={'second'} songs={songs} values={values} setFieldValue={setFieldValue} />
-          <Selector place={'third'} songs={songs} values={values} setFieldValue={setFieldValue} />
 
+  const doNotMatch = (field: 'first' | 'second' | 'third' | 'heard') => ({
+    is: (val: string) => val && val !== '',
+    then: yup.string().notOneOf([yup.ref(field)], `You can't vote for the same song twice`),
+  })
+
+  const voteRules = yup.object().shape({
+    first: yup.string().when('second', doNotMatch('second')).when('third', doNotMatch('third')),
+    second: yup.string().when('third', doNotMatch('third')),
+    third: yup.string(),
+    heard: yup.array().of(
+      yup.string()).test('not-voting', "You can't vote for songs you've heard", (value, context) => {
+        const { first, second, third } = context.parent
+        return (
+          !value?.includes(first) && !value?.includes(second) && !value?.includes(third)
+        )
+      })
+  })
+
+  const printErrors = (errors: any) => {
+    const e: string[] = Object.values(errors)
+    return e.length > 1 ? e[0] : e
+  }
+
+  const renderHeardFields = (
+    heard: string[],
+    setFieldValue: (field: string, value: any, shouldValidate?: boolean | undefined) => void,
+    arrayHelpers: FieldArrayRenderProps
+  ) =>
+    heard.map((_, i) => (
+      <div className="heard-selector" key={`heard-selector-${i}`}>
+        <Selector place="heard" songs={songs} setFieldValue={setFieldValue} index={i} />
+        {i === heard.length - 1 ? (
           <div className="vote-right-buttons">
-            {/* <button className="vote-button" type="button">
+            <button
+              className="vote-button close"
+              type="button"
+              onClick={() => arrayHelpers.push('')}
+            >
               <FontAwesomeIcon icon={faPlusCircle} />
-            </button> */}
-            <button className="vote-button" type="submit">
-              <FontAwesomeIcon icon={faVoteYea} />
+            </button>
+            <button className="vote-button add" type="submit">
+              <FontAwesomeIcon icon={faVoteYea}/>
             </button>
           </div>
+        ) : (
+          <button
+            className="vote-button submit"
+            type="button"
+            onClick={() => arrayHelpers.remove(i)}
+          >
+            <FontAwesomeIcon icon={faTimesCircle} color="#707070" />
+          </button>
+        )}
+      </div>
+    ))
+
+  return (
+    <Formik
+      initialValues={{ first: '', second: '', third: '', heard: [''] }}
+      onSubmit={(values) => submitVotes(id, values, today)}
+      validationSchema={voteRules}
+    >
+      {({ values, setFieldValue, errors, touched }) => (
+        <Form className='Votes'>
+          <Selector place={'first'} songs={songs} setFieldValue={setFieldValue} />
+          <Selector place={'second'} songs={songs} setFieldValue={setFieldValue} />
+          <Selector place={'third'} songs={songs} setFieldValue={setFieldValue} />
+          <FieldArray 
+            name="heard" 
+            render={(arrayHelpers) => (
+            <>{renderHeardFields(values.heard, setFieldValue, arrayHelpers)}</>
+          )}
+          />
+          {Object.values(errors).length > 0 && <div>{printErrors(errors)}</div>}
         </Form>
       )}
     </Formik>
